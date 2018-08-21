a6aaa67eb50a0192f4247f424728370347437738
tonystig <tony.stig@icloud.com>
2018-08-21 18:48:00 +0800

    include websocket

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,71 @@
-function initSocket(socketURL){
-    var socket = io(socketURL);
+var socketURL = 'https://tanglemonitor.com:4434';
+var socket = io(socketURL);
+
+var tx_list = [];
+
+initSocket();
+
+function initSocket(){
     socket.on('connect',function(){
-        console.log('Successfully connected to Websocket..');
+        if(socket.connected){
+            console.log('Successfully connected to Websocket.. ID:',socket.id);
+        }else{
+            console.log('something worng..');
+        }
     })
-    // socket.on('newTX',function(newTX){
-    //     console.log(newTX);
-    // })
-    // socket.on('disconnect', reason => {
-    //     console.log('WebSocket disconnect');
-    //   });
 }
-var url = 'https://tanglemonitor.com:4434';
-initSocket(url);
+
+function start(){
+    readNewTx();
+    readUpdateTx();
+    console.log(tx_list);
+}
+
+function pause(){
+    socket.on('disconnect',function(){});
+}
+
+function readNewTx(){
+    socket.on('newTX',function(newTX){
+        tx_list.push(newTX);
+    })
+}
+
+function readUpdateTx(){
+    socket.on('update', function(update) {
+        updateTXStatus(update, 'txConfirmed');
+    });
+}
+
+function updateTXStatus(update, updateType){
+    console.log("in update");
+    var txHash = update.hash;
+    var milestoneType = update.milestone;
+    var confirmationTime = update.ctime;
+
+    var hashIndex = tx_list.findIndex(tx => tx.hash === txHash);
+    console.log(hashIndex)
+    if (hashIndex !== -1 && tx_list[hashIndex] !== undefined) {
+        if (updateType === 'txConfirmed' || updateType === 'Milestone') {
+            tx_list[hashIndex].ctime = confirmationTime;
+            tx_list[hashIndex].confirmed = true;
+        }
+        if (updateType === 'Milestone') {
+            tx_list[hashIndex].milestone = milestoneType;
+        }
+        if (updateType === 'Reattach') {
+            tx_list[hashIndex].reattached = true;
+        }
+    }else{
+        console.log(`${updateType === 'Milestone' ? 'Milestone' : 'TX'} not found in local DB - Hash: ${txHash} | updateType: ${updateType}`);
+    }
+    consoleTx();
+}
+
+function consoleTx(){
+    for(var i=0;i<tx_list.length;i++){
+        if(tx_list[i].confirmed){
+            console.log("@@@@@@@@@@@",tx_list[i])
+        }
+    }
+}
