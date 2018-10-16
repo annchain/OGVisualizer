ad45c49913a54def05883d877d1e18de6d2f30ba
tonystig <tony.stig@icloud.com>
2018-10-16 14:37:40 +0800

    config update

--- public/js/domain/config.js
+++ public/js/domain/config.js
@@ -13,7 +13,8 @@ var config = {
         "authMechanism":""
     },
     "websocket":{
-        "host":"localhost:8002/ws"
+        // "host":"localhost:8002/ws"
+        "host":"10.253.169.129:8002/ws"
         //"host":"https://tanglemonitor.com:4434"
     }
 }
